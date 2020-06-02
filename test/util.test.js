da80dcbae6fbb7a501bcdb6864596a299ba424d2
Meianna Oeser <meianna.oeser@gmail.com>
2020-06-01 19:58:49 -0700

    added minus function

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,4 @@
-const { add } = require("../util/util");
+const { add, minus } = require("../util/util");
 
 describe("Add", () => {
   describe("Success", () => {
@@ -6,10 +6,31 @@ describe("Add", () => {
       expect(add(2, 2)).toBe(4);
     });
   });
-
   describe("Fail", () => {
     it("Should throw an error if the first arugment is not a number", () => {
       expect(() => add("2", 2)).toThrowError();
+    });
+    it("Should throw an error if the first arugment is not a number", () => {
+      expect(() => add(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no arguments were received", () => {
+      expect(() => add()).toThrowError();
+    });
+  });
+});
+
+describe("Minus", () => {
+  describe("Success", () => {
+    it("Should return the first argument minus the second", () => {
+      expect(minus(4, 2)).toBe(2);
+    });
+  });
+  describe("Fail", () => {
+    it("Should return an error if the first argument is not a number", () => {
+      expect(() => minus("2", 2)).toThrowError();
+    });
+    it("Should return an error if the first argument is not a number", () => {
+      expect(() => minus(2, "2")).toThrowError();
     });
   });
 });
